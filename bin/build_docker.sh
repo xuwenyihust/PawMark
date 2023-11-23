e49153d0a5b07e82c720e524041097ce941f4fdc
xuwenyihust <wenyixu101@gmail.com>
2023-11-23 20:02:07 +0800

    Update sidecar ui service.

--- bin/build_docker.sh
+++ bin/build_docker.sh
@@ -4,7 +4,7 @@ DOCKER_ACCOUNT="wenyixu101"
 DOCKER_PATH="./kubernetes"
 DOCKERFILE="Dockerfile.spark-ui"
 
-DOCKER_IMAGE="kubectl:0.0.2"
+DOCKER_IMAGE="kubectl:0.0.3"
 
 docker build --no-cache . -t ${DOCKER_ACCOUNT}/${DOCKER_IMAGE} -f ${DOCKER_PATH}/${DOCKERFILE}
 docker push ${DOCKER_ACCOUNT}/${DOCKER_IMAGE}
