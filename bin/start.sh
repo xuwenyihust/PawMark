bee77c5e4b0c2266a4267bbada5ef94a840f8cfd
xuwenyihust <wenyixu101@gmail.com>
2023-11-26 13:01:30 +0800

    Update start.sh and spark-driver-template.yaml

--- bin/start.sh
+++
@@ -1,6 +0,0 @@
-source bin/submit_spark_app.sh \
-    --image "wenyixu101/word-count:test-3" \
-    --name "word-count" \
-    --main "WordCount" \
-    --jar "/opt/spark/app.jar"
-
--- bin/submit_spark_app.sh
+++ bin/submit_spark_app.sh
@@ -81,6 +81,57 @@ arg_check() {
     echo "args: $args"
 }
 
+submit() {
+    APP_NAME=$name
+    PROJECT_HOME=$(pwd)
+
+    # Define your Spark application's main class
+    # MAIN_CLASS="org.apache.spark.examples.SparkPi"
+    MAIN_CLASS=$main
+
+    # APP_JAR="/opt/spark/examples/jars/spark-examples_${SCALA_VERSION}-${SPARK_VERSION}.jar"
+    APP_JAR=$jar
+    DRIVER_TEMPLATE="$PROJECT_HOME/kubernetes/spark-driver-template.yaml"
+    CONTAINER_NAME="spark-kubernetes-driver"
+
+    # Set other Spark configurations and application arguments
+    APP_ARGS=$args
+
+    K8S_NAMESPACE="spark-dev"
+    SERVICE_ACCOUNT="spark"
+    # DOCKER_IMAGE="apache/spark:${SPARK_VERSION}"
+    DOCKER_IMAGE=$image
+    # Export the variable so that it can be used in the awk command below
+    export DOCKER_IMAGE
+
+    FILE_UPLOAD_PATH="/tmp/spark-uploads"
+
+    echo "Replacing the image for spark-kubernetes-driver container...\n"
+    awk '/- name: spark-kubernetes-driver/{flag=1} flag && /image:/{sub(/image:.*/, "image: " ENVIRON["DOCKER_IMAGE"]); flag=0} 1' $DRIVER_TEMPLATE > tmpfile && mv tmpfile $DRIVER_TEMPLATE
+    cat $DRIVER_TEMPLATE
+
+    # The command to submit the Spark job
+    spark-submit \
+        --class $MAIN_CLASS \
+        --master k8s://$KUBERNETES_API_SERVER_HOST:$KUBERNETES_API_SERVER_PORT \
+        --deploy-mode cluster \
+        --driver-cores 1 \
+        --driver-memory 1g \
+        --num-executors 1 \
+        --executor-cores 1 \
+        --executor-memory 1g \
+        --name $APP_NAME \
+        --conf spark.kubernetes.driver.podTemplateFile=$DRIVER_TEMPLATE \
+        --conf spark.kubernetes.container.image=$DOCKER_IMAGE \
+        --conf spark.kubernetes.namespace=$K8S_NAMESPACE \
+        --conf spark.kubernetes.authenticate.driver.serviceAccountName=$SERVICE_ACCOUNT \
+        --conf spark.kubernetes.authenticate.executor.serviceAccountName=$SERVICE_ACCOUNT \
+        --conf spark.kubernetes.file.upload.path=$FILE_UPLOAD_PATH \
+        --conf spark.kubernetes.driver.label.app=spark \
+        local://$APP_JAR \
+        $APP_ARGS
+}
+
 arg_check "$@"
 parse_status=$?
 
@@ -89,54 +140,12 @@ if [ $parse_status -ne 0 ]; then
 # elif
 
 else
-
+    echo "Submitting Spark application..."
+    submit "$@"
 fi
 
 
-# Set the path to your spark-submit binary if not in PATH
-# SPARK_SUBMIT="/path/to/spark/bin/spark-submit"
 
-# APP_NAME="spark-app"
-# PROJECT_HOME=$(pwd)
-
-# # Define your Spark application's main class
-# # MAIN_CLASS="org.apache.spark.examples.SparkPi"
-# MAIN_CLASS="WordCount"
-
-# # APP_JAR="/opt/spark/examples/jars/spark-examples_${SCALA_VERSION}-${SPARK_VERSION}.jar"
-# APP_JAR="/opt/spark/app.jar"
-# DRIVER_TEMPLATE="$PROJECT_HOME/kubernetes/spark-driver-template.yaml"
-
-# # Set other Spark configurations and application arguments
-# APP_ARGS="1000"
-
-# K8S_NAMESPACE="spark-dev"
-# SERVICE_ACCOUNT="spark"
-# # DOCKER_IMAGE="apache/spark:${SPARK_VERSION}"
-# DOCKER_IMAGE="wenyixu101/word-count:test-3"
-
-# FILE_UPLOAD_PATH="/tmp/spark-uploads"
-
-# # The command to submit the Spark job
-# spark-submit \
-#     --class $MAIN_CLASS \
-#     --master k8s://$KUBERNETES_API_SERVER_HOST:$KUBERNETES_API_SERVER_PORT \
-#     --deploy-mode cluster \
-#     --driver-cores 1 \
-#     --driver-memory 1g \
-#     --num-executors 1 \
-#     --executor-cores 1 \
-#     --executor-memory 1g \
-#     --name $APP_NAME \
-#     --conf spark.kubernetes.driver.podTemplateFile=$DRIVER_TEMPLATE \
-#     --conf spark.kubernetes.container.image=$DOCKER_IMAGE \
-#     --conf spark.kubernetes.namespace=$K8S_NAMESPACE \
-#     --conf spark.kubernetes.authenticate.driver.serviceAccountName=$SERVICE_ACCOUNT \
-#     --conf spark.kubernetes.authenticate.executor.serviceAccountName=$SERVICE_ACCOUNT \
-#     --conf spark.kubernetes.file.upload.path=$FILE_UPLOAD_PATH \
-#     --conf spark.kubernetes.driver.label.app=spark \
-#     local://$APP_JAR \
-#     $APP_ARGS
 
 
 
