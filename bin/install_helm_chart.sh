86412dd4b565e6b99b1f1bb2cd553dbc0a47010c
xuwenyihust <wenyixu101@gmail.com>
2023-12-20 15:29:04 +0800

    Update Helm release names and versions

--- bin/install_helm_chart.sh
+++ bin/install_helm_chart.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Check if the release exists
-if helm list -n $GKE_NAMESPACE | grep -q $RELEASE_NAME; then
-    echo "Upgrading release '$RELEASE_NAME'..."
+if helm list -n $GKE_NAMESPACE | grep -q $RELEASE_DATA_PLATFORM_NAME; then
+    echo "Upgrading release '$RELEASE_DATA_PLATFORM_NAME'..."
 
     # Upgrade the Helm release
-    helm upgrade $RELEASE_NAME $CHART_NAME \
+    helm upgrade $RELEASE_DATA_PLATFORM_NAME $CHART_DATA_PLATFORM_NAME \
         --namespace $GKE_NAMESPACE \
-        --version $CHART_VERSION \
+        --version $CHART_DATA_PLATFORM_VERSION \
         --install  # The --install flag ensures it installs if not present
 
     echo "Upgrade completed."
 else
-    echo "Release '$RELEASE_NAME' not found. Installing..."
+    echo "Release '$RELEASE_DATA_PLATFORM_NAME' not found. Installing..."
     # Install the Helm chart as a new release
-    helm install $RELEASE_NAME $CHART_NAME \
+    helm install $RELEASE_DATA_PLATFORM_NAME $CHART_DATA_PLATFORM_NAME \
         --namespace $GKE_NAMESPACE \
-        --version $CHART_VERSION \
+        --version $CHART_DATA_PLATFORM_VERSION \
         --create-namespace  # Creates the namespace if it doesn't exist
 fi
--- bin/uninstall_helm_chart.sh
+++ bin/uninstall_helm_chart.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Check if the release exists
-if helm list -n $NAMESPACE | grep -q $RELEASE_NAME; then
-    helm uninstall $RELEASE_NAME -n $NAMESPACE
+if helm list -n $GKE_NAMESPACE | grep -q $RELEASE_DATA_PLATFORM_NAME; then
+    helm uninstall $RELEASE_DATA_PLATFORM_NAME -n $GKE_NAMESPACE
 else
-    echo "Release '$RELEASE_NAME' not found."
+    echo "Release '$RELEASE_DATA_PLATFORM_NAME' not found."
+fi
+
+if helm list -n $GKE_NAMESPACE | grep -q $RELEASE_SPARK_UI_NAME; then
+    helm uninstall $RELEASE_SPARK_UI_NAME -n $GKE_NAMESPACE
+else
+    echo "Release '$RELEASE_SPARK_UI_NAME' not found."
 fi
