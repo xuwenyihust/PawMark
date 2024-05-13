1132c7d28bdcf76d8ba6a74bc1810b652eea8b84
xuwenyihust <wenyixu101@gmail.com>
2024-05-13 14:11:04 +0800

    Update X-Frame-Options header in Jupyter Notebook config

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Sidebar from './Sidebar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <Sidebar />
+        <div style={{ flex: 1, padding: '10px' }}>
+          <Switch>
+            <Route path="/create-notebook">
+              {/* Component to handle creating a new notebook */}
+              <h1>Create a new Notebook</h1>
+            </Route>
+            <Route path="/notebook">
+              {/* Iframe for Jupyter Notebook */}
+              <iframe src="http://localhost:8888" width="100%" height="800px" style={{ border: 'none' }}></iframe>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
