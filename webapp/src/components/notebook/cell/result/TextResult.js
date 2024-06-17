9c1d7a2f44ba79b80bc27f47b60c13062124b0ca
Wenyi Xu <wenyixu101@gmail.com>
2024-06-17 22:19:13 +0800

    Update notebook execution timestamps and fix styling in TextResult.js (#210)

--- webapp/src/components/notebook/cell/result/TextResult.js
+++ webapp/src/components/notebook/cell/result/TextResult.js
@@ -1,9 +1,9 @@
-import { Typography } from '@mui/material';
+import { Typography, Card } from '@mui/material';
 
 
 function TextResult(output) {
   return (
-    <Typography 
+    <div 
       variant="body1"
       component="div"
       sx={{
@@ -13,14 +13,64 @@ function TextResult(output) {
         fontSize: '14px',
         fontWeight: 'bold',
         textAlign: 'left',
-        marginLeft: 10,
+        marginLeft: 0,
         marginRight: 1,
         marginTop: 0,
         marginBottom: 1,
         marginRight: 1,
       }}>
-      <div dangerouslySetInnerHTML={{ __html: output.data['text/html'] }} />
-    </Typography>
+        { output.data['text/html'] ? 
+        <Card
+        elevation={0}
+        sx={{
+          width: '85%',
+          marginTop: 0,
+          marginBottom: 1,
+          marginLeft: 10,
+          marginRight: 1,
+          border: 0.1,
+          borderColor: 'lightgrey',
+          backgroundColor: 'rgba(0, 0, 0, 0.01)',
+        }}>
+          <Typography 
+            sx={{
+              marginLeft: 2}}
+            dangerouslySetInnerHTML={{ __html: output.data['text/html'] }} />
+        </Card>
+        :
+          <Card
+            elevation={0}
+            sx={{
+              width: '85%',
+              marginTop: 0,
+              marginBottom: 1,
+              marginLeft: 10,
+              marginRight: 1,
+              border: 0.1,
+              borderColor: 'lightgrey',
+              backgroundColor: 'rgba(0, 0, 0, 0.01)',
+            }}>
+            <Typography 
+              variant="body1"
+              component="div"
+              sx={{
+                color: 'black',
+                width: '85%',
+                fontFamily: 'Arial',
+                fontSize: '14px',
+                fontWeight: 'bold',
+                textAlign: 'left',
+                marginLeft: 1,
+                marginRight: 1,
+                marginTop: 1,
+                marginBottom: 1,
+                marginRight: 1,
+              }}>
+              {output.data['text/plain']}
+            </Typography>
+          </Card>
+        }
+    </div>
   );
 }
 
