793e7b2461ed1076703e81a609215bbb20a152b9
xuwenyihust <wenyixu101@gmail.com>
2024-06-18 23:26:20 +0800

    Update ReactMarkdown and remark-gfm versions, add StringUtils.js utility class, and refactor CodeResult and Cell components

---
+++ webapp/src/utils/StringUtils.js
@@ -0,0 +1,27 @@
+class StringUtils {
+  constructor() {
+    console.log('StringUtils constructor');
+  }
+
+  static isJupyterTable(str) {
+    const regex = /\+\-{3,}\+/;
+    return regex.test(str);
+  }
+
+  static convertJupyterTableToMarkdownTable = (str) => {
+    const rows = str.split('\n').filter(row => row.startsWith('|'));
+    const markdownRows = rows.map(row => row.split('|').slice(1, -1).join('|'));
+    const header = markdownRows.shift();
+    let separator = header.replace(/[^|]/g, '-');
+    if (!separator.includes('|')) {
+      separator = `|${separator}|`;
+    }
+
+    const res = [header, separator, ...markdownRows].join('\n');
+    console.log('Converted Jupyter table to markdown table:', res);
+    return res;
+  }
+}
+
+export default StringUtils;
+
