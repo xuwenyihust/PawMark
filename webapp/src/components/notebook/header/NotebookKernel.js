75791c2f7fd6818e65e6fbac6718f0422ad3b210
xuwenyihust <wenyixu101@gmail.com>
2024-06-21 15:14:31 +0800

    Refactor code to use PawMarkSparkSession instead of DataPulseSparkSession in startup.py and NotebookKernel.js

--- webapp/src/components/notebook/header/NotebookKernel.js
+++ webapp/src/components/notebook/header/NotebookKernel.js
@@ -1,10 +1,29 @@
-import { Tooltip, Box, Button } from '@mui/material';
+import { Tooltip, Box, Button, Menu, MenuItem } from '@mui/material';
+import { useState } from 'react';
 import { JupyterKernelIcon } from '@datalayer/icons-react';
-
+import { VscTriangleDown } from "react-icons/vsc";
+import LoadingButton from '@mui/lab/LoadingButton';
+import NotebookModel from '../../../models/NotebookModel';
+import config from '../../../config';
 
 const NotebookKernel = ({
   kernelId
 }) => {
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isRestarting, setIsRestarting] = useState(false);
+
+  const handleRestartKernel = async () => {
+    try {
+      setIsRestarting(true);
+      setMenuOpen(false);
+      await NotebookModel.restartKernel(config.jupyterBaseUrl, kernelId);
+      setIsRestarting(false);
+    } catch (error) {
+      console.error('Failed to restart kernel:', error);
+    }
+  }
 
   return (
     <Box sx={{ 
@@ -20,16 +39,48 @@ const NotebookKernel = ({
             style={{ fontSize: '10px', padding: '3px 6px' }}>
             Not Connected
           </Button> : 
+
+          isRestarting ?
+          <LoadingButton 
+            variant="contained" 
+            size="small"
+            loading
+            style={{ fontSize: '10px', padding: '3px 6px' }}>
+              Restarting...
+          </LoadingButton> :
+
           <Tooltip title={kernelId}>
             <Button 
               variant="contained" 
               size="small"
               startIcon={<JupyterKernelIcon />}
+              endIcon={<VscTriangleDown 
+                style={{ 
+                  fontSize: '10px', 
+                  marginLeft: 0, 
+                  marginRight: 0 }}
+                />}
+              onClick={(e) => { setMenuOpen(true); setAnchorEl(e.currentTarget);}}
               style={{ fontSize: '10px', padding: '3px 6px' }}>
               Connected
             </Button>
           </Tooltip>
-          }
+        }
+
+        <Menu
+          open={menuOpen}
+          onClose={() => setMenuOpen(false)}
+          anchorEl={anchorEl}
+          >
+          <MenuItem 
+            style={{
+              fontSize: '12px',
+              width: '115px',
+            }}
+            onClick={handleRestartKernel}>
+              Restart Kernel
+          </MenuItem>
+        </Menu>
     </Box>
   );
 }
