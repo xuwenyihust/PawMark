f7ab8f28daf542dd13e20c68f06c153f88924ac7
xuwenyihust <wenyixu101@gmail.com>
2024-07-04 13:08:36 +0800

    Refactor notebook creation API endpoint and model to use 'name' and 'path' instead of 'notebookName' in test_notebook_service.py and fix test_create_and_get_notebook test case

--- server/tests/services/test_notebook_service.py
+++ server/tests/services/test_notebook_service.py
@@ -38,12 +38,13 @@ class NotebookServiceTestCase(unittest.TestCase):
 
   def test_create_and_get_notebook(self):
     with self.app.app_context():
-      create_response = Notebook.create_notebook_with_init_cells(notebook_name='Notebook.ipynb', notebook_path='')
-      # print(create_response)
+      Notebook.create_notebook_with_init_cells(notebook_name='Notebook.ipynb', notebook_path='')
 
-      print(Notebook.get_notebook_by_path(notebook_path='work/Notebook.ipynb')[0])
-      # notebooks = json.loads(Notebook.get_notebook_by_path(notebook_path='work/Notebook.ipynb'))
-      # self.assertEqual(len(notebooks), 1)
-      # self.assertEqual(notebooks['name'], 'Notebook.ipynb')
-      # self.assertEqual(notebooks['path'], 'work/Notebook.ipynb')
+      notebooks = json.loads(Notebook.get_notebook_by_path(notebook_path='work/Notebook.ipynb')[0])
+      status_code = Notebook.get_notebook_by_path(notebook_path='work/Notebook.ipynb')[1]
+
+      self.assertEqual(status_code, 200)
+      self.assertEqual(len(notebooks), 1)
+      self.assertEqual(notebooks['name'], 'Notebook.ipynb')
+      self.assertEqual(notebooks['path'], 'work/Notebook.ipynb')
       
