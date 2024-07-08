42dad8da3001fdf97fde588ee8c38c1820e151d2
xuwenyihust <wenyixu101@gmail.com>
2024-07-08 14:51:41 +0800

    Fix JSON parsing issue and update print statement in test_directory_service.py

--- server/tests/services/test_directory_service.py
+++ server/tests/services/test_directory_service.py
@@ -43,8 +43,10 @@ class DirectoryServiceTestCase(unittest.TestCase):
       status_code_0 = response_0[1]
 
       print(response_0[0])
-      # print(response_0[0]['name'])
+      print(response_0[0].status_code)
       print(response_0[0].json())
+      # print(response_0[0]['name'])
+      # print(response_0[0].json())
 
       # content_0 = [x for x in response_0[0].json() if x['name'] == 'updated_name']
       # self.assertEqual(status_code_0, 200)
