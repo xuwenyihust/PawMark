cb9d8e39742be3a6e92de5039a7b872cd3f79555
xuwenyihust <wenyixu101@gmail.com>
2024-07-09 13:51:30 +0800

    Refactor notebook retrieval in notebook.py

--- server/app/services/notebook.py
+++ server/app/services/notebook.py
@@ -50,10 +50,7 @@ class Notebook:
         response=json.dumps({'message': 'Error getting notebook from DB: ' + str(e)}), 
         status=404)
 
-    return Response(
-        response=response.json(), 
-        status=200
-      )
+    response
 
   @staticmethod
   def create_notebook(notebook_name: str = None, notebook_path: str = None) -> None:
