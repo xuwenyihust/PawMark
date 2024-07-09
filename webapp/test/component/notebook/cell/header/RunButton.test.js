a15f711641e25caae1550d95149e865bdafd9586
xuwenyihust <wenyixu101@gmail.com>
2024-07-09 22:19:18 +0800

    Update import path in RunButton.test.js

--- webapp/test/component/notebook/cell/header/RunButton.test.js
+++ webapp/test/component/notebook/cell/header/RunButton.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import RunButton from '../../../../../src/components/notebook/header/RunButton';
+import RunButton from '../../../../../src/components/notebook/content/cell/header/RunButton';
 
 test('calls onClick prop when clicked', () => {
   const handleClick = jest.fn();
