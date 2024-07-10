211c95b26a3037f744f4d13d016237e9559db293
xuwenyihust <wenyixu101@gmail.com>
2024-07-10 18:07:42 +0800

    Update session service to include create_session method in session.py, refactor test_session_service.py and session.py, and add session creation functionality

--- server/tests/services/test_session_service.py
+++ server/tests/services/test_session_service.py
@@ -14,10 +14,16 @@ class SessionServiceTestCase(unittest.TestCase):
 
   def test_get_all_sessions(self):
     with self.app.app_context():
-      # Get all sessions
-      response_0 = Session.get_all_sessions()
+      # Create Notebook
+      response_0 = Notebook.create_notebook_with_init_cells(notebook_name='Notebook_0.ipynb', notebook_path='')
       self.assertEqual(response_0.status_code, 200)
-      print(response_0.data)
+
+      notebook_0 = json.loads(response_0.data.decode('utf-8'))
+      notebook_path_0 = notebook_0['path']
+
+      # Create session
+      response_1 = Session.create_session(notebook_path_0)
+      self.assertEqual(response_1.status_code, 200)
 
       # Create Notebook
       response_1 = Notebook.create_notebook_with_init_cells(notebook_name='Notebook_1.ipynb', notebook_path='')
@@ -30,21 +36,16 @@ class SessionServiceTestCase(unittest.TestCase):
       response_2 = Session.create_session(notebook_path_1)
       self.assertEqual(response_2.status_code, 200)
 
-      # Create Notebook
-      response_2 = Notebook.create_notebook_with_init_cells(notebook_name='Notebook_2.ipynb', notebook_path='')
-      self.assertEqual(response_2.status_code, 200)
-
-      notebook_2 = json.loads(response_2.data.decode('utf-8'))
-      notebook_path_2 = notebook_2['path']
-
-      # Create session
-      response_3 = Session.create_session(notebook_path_2)
-      self.assertEqual(response_3.status_code, 200)
-
       # Get all sessions
-      response_4 = Session.get_all_sessions()
-      self.assertEqual(response_4.status_code, 200)
-      print(response_4.data)
+      response_3 = Session.get_all_sessions()
+      self.assertEqual(response_3.status_code, 200)
+      sessions = json.loads(response_3.data.decode('utf-8'))
+
+      session_0 = [x for x in sessions if x["path"] == notebook_path_0]
+      session_1 = [x for x in sessions if x["path"] == notebook_path_1]
+
+      self.assertEqual(len(session_0), 1)
+      self.assertEqual(len(session_1), 1)
 
     
   def test_create_session(self):
