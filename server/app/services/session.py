0f9ca237708cea481c325cb063cd2ac004192bf6
xuwenyihust <wenyixu101@gmail.com>
2024-07-10 16:34:49 +0800

    Update session service to include create_session method in session.py

---
+++ server/app/services/session.py
@@ -0,0 +1,47 @@
+import logging
+from flask import Response
+import requests
+import json
+from flask import current_app as app
+
+
+logger = logging.getLogger(__name__)
+
+
+class Session:
+
+  @staticmethod
+  def create_session(notebook_path: str) -> None:
+    jupyter_api_path = app.config['JUPYTER_API_PATH']
+
+    path = f"{jupyter_api_path}/api/sessions"
+
+    data = {
+      "notebook": {
+        "path": notebook_path
+      },
+      "kernel": {
+        "id": None, 
+        "name": "python3"
+      }
+    }
+
+    try:
+      response = requests.post(
+        path,
+        json=data
+      )
+    except Exception as e:
+      return Response(
+        response=json.dumps({'message': 'Error creating session in Jupyter Server: ' + str(e)}), 
+        status=404)
+    
+    return Response(
+      response=response, 
+      status=200,
+      mimetype='application/json'
+    )
+
+    
+
+
---
+++ server/tests/services/test_session_service.py
@@ -0,0 +1,25 @@
+import unittest
+from flask_cors import CORS
+from run import create_app
+import json
+from app.services.session import Session
+from app.services.notebook import Notebook
+
+
+class SessionServiceTestCase(unittest.TestCase):
+
+  def setUp(self):
+    self.app = create_app()
+    self.client = self.app.test_client()
+    
+  def test_create_session(self):
+    with self.app.app_context():
+      # Create Notebook
+      response_0 = Notebook.create_notebook_with_init_cells(notebook_name='Notebook.ipynb', notebook_path='')
+      self.assertEqual(response_0.status_code, 200)
+
+      print(">>>>>>>")
+      print(response_0)
+
+      # Create session
+
