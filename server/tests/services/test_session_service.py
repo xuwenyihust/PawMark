06f7ad53b4198dc22cb8f534adfc8a89d18a8373
xuwenyihust <wenyixu101@gmail.com>
2024-07-27 16:08:37 +0800

    Refactor test_session_service.py to add user model and test case for getting all sessions

--- server/tests/services/test_session_service.py
+++ server/tests/services/test_session_service.py
@@ -64,60 +64,74 @@ class SessionServiceTestCase(unittest.TestCase):
       self.assertEqual(len(session_0), 1)
       self.assertEqual(len(session_1), 1)
 
-#   def test_get_session_by_path(self):
-#     with self.app.app_context():
-#       # Create Notebook
-#       response_0 = Notebook.create_notebook_with_init_cells(notebook_name='Notebook_0.ipynb', notebook_path='')
-#       self.assertEqual(response_0.status_code, 200)
+  def test_get_session_by_path(self):
+    with self.app.app_context():
+      user = UserModel(name='testuser0', email='testuser0@example.com')
+      password = 'test_password'
+      user.set_password(password)
+      db.session.add(user)
+      db.session.commit()
+      g.user = user
 
-#       notebook_0 = json.loads(response_0.data.decode('utf-8'))
-#       notebook_path_0 = notebook_0['path']
+      # Create Notebook
+      response_0 = Notebook.create_notebook_with_init_cells(notebook_name='Notebook_0.ipynb', notebook_path='')
+      self.assertEqual(response_0.status_code, 200)
 
-#       # Create session
-#       response_1 = Session.create_session(notebook_path_0)
-#       self.assertEqual(response_1.status_code, 200)
+      notebook_0 = json.loads(response_0.data.decode('utf-8'))
+      notebook_path_0 = notebook_0['path']
 
-#       # Create Notebook
-#       response_1 = Notebook.create_notebook_with_init_cells(notebook_name='Notebook_1.ipynb', notebook_path='')
-#       self.assertEqual(response_1.status_code, 200)
+      # Create session
+      response_1 = Session.create_session(notebook_path_0)
+      self.assertEqual(response_1.status_code, 200)
 
-#       notebook_1 = json.loads(response_1.data.decode('utf-8'))
-#       notebook_path_1 = notebook_1['path']
+      # Create Notebook
+      response_1 = Notebook.create_notebook_with_init_cells(notebook_name='Notebook_1.ipynb', notebook_path='')
+      self.assertEqual(response_1.status_code, 200)
 
-#       # Create session
-#       response_2 = Session.create_session(notebook_path_1)
-#       self.assertEqual(response_2.status_code, 200)
+      notebook_1 = json.loads(response_1.data.decode('utf-8'))
+      notebook_path_1 = notebook_1['path']
 
-#       # Get session by path
-#       response_3 = Session.get_session_by_path(notebook_path_0)
-#       self.assertEqual(response_3.status_code, 200)
-#       session_0 = json.loads(response_3.data.decode('utf-8'))
+      # Create session
+      response_2 = Session.create_session(notebook_path_1)
+      self.assertEqual(response_2.status_code, 200)
 
-#       response_4 = Session.get_session_by_path(notebook_path_1)
-#       self.assertEqual(response_4.status_code, 200)
-#       session_1 = json.loads(response_4.data.decode('utf-8'))
+      # Get session by path
+      response_3 = Session.get_session_by_path(notebook_path_0)
+      self.assertEqual(response_3.status_code, 200)
+      session_0 = json.loads(response_3.data.decode('utf-8'))
 
-#       response_5 = Session.get_session_by_path("fake_path")
-#       self.assertEqual(response_5.status_code, 404)
+      response_4 = Session.get_session_by_path(notebook_path_1)
+      self.assertEqual(response_4.status_code, 200)
+      session_1 = json.loads(response_4.data.decode('utf-8'))
 
-#       self.assertEqual(session_0["path"], notebook_path_0)
-#       self.assertEqual(session_1["path"], notebook_path_1)  
+      response_5 = Session.get_session_by_path("fake_path")
+      self.assertEqual(response_5.status_code, 404)
 
-#   def test_create_session(self):
-#     with self.app.app_context():
-#       # Create Notebook
-#       response_0 = Notebook.create_notebook_with_init_cells(notebook_name='Notebook.ipynb', notebook_path='')
-#       self.assertEqual(response_0.status_code, 200)
+      self.assertEqual(session_0["path"], notebook_path_0)
+      self.assertEqual(session_1["path"], notebook_path_1)  
 
-#       notebook = json.loads(response_0.data.decode('utf-8'))
-#       notebook_path = notebook['path']
+  def test_create_session(self):
+    with self.app.app_context():
+      user = UserModel(name='testuser0', email='testuser0@example.com')
+      password = 'test_password'
+      user.set_password(password)
+      db.session.add(user)
+      db.session.commit()
+      g.user = user
+      
+      # Create Notebook
+      response_0 = Notebook.create_notebook_with_init_cells(notebook_name='Notebook.ipynb', notebook_path='')
+      self.assertEqual(response_0.status_code, 200)
 
-#       # Create session
-#       response_1 = Session.create_session(notebook_path)
-#       self.assertEqual(response_1.status_code, 200)
+      notebook = json.loads(response_0.data.decode('utf-8'))
+      notebook_path = notebook['path']
 
-#       session = json.loads(response_1.data.decode('utf-8'))
-#       self.assertIsNotNone(session["id"])
-#       self.assertIsNotNone(session["kernel"]["id"])
-#       self.assertEquals(session["kernel"]["name"], "python3")
+      # Create session
+      response_1 = Session.create_session(notebook_path)
+      self.assertEqual(response_1.status_code, 200)
+
+      session = json.loads(response_1.data.decode('utf-8'))
+      self.assertIsNotNone(session["id"])
+      self.assertIsNotNone(session["kernel"]["id"])
+      self.assertEquals(session["kernel"]["name"], "python3")
 
