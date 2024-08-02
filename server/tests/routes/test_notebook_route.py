6fd5229232ba319d39a7c1700509ad47af77dfb6
xuwenyihust <wenyixu101@gmail.com>
2024-08-02 16:44:16 +0800

    Fix incorrect URL in test_notebook_route.py

--- server/tests/routes/test_notebook_route.py
+++ server/tests/routes/test_notebook_route.py
@@ -252,5 +252,5 @@ class NotebookRouteTestCase(unittest.TestCase):
         notebook_id=notebook.id)
 
       # Get spark app by notebook path
-      response_3 = self.client.get('/notebook/spark_app/work/test_get_spark_app_by_notebook_path_directory/test_notebook.ipynb', headers=headers)
+      response_3 = self.client.get('work/test_get_spark_app_by_notebook_path_directory/test_notebook.ipynb', headers=headers)
       self.assertEqual(response_3.status_code, 200)   
